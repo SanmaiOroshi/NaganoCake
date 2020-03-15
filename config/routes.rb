baef9914ded0a5ea80dc0acc28993362e896d386
funeace <funeace@gmail.com>
2020-03-15 02:36:29 +0000

    [Start]NaganoCake

---
+++ config/routes.rb
@@ -0,0 +1,41 @@
+Rails.application.routes.draw do
+  # ECサイトのrouting
+  root 'homes#top'
+  devise_for :admins
+  devise_for :users
+
+  resources :products, only: [:index,:show]
+  resources :carts, only: [:index,:edit,:update,:destroy] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
+  resources :orders, only: [:index,:new,:create,:show] do
+    collection do
+      get 'input'
+      get 'complete'
+    end
+  end
+  resources :users, only: [:show,:edit,:update] do
+    member do
+      get 'quit'
+    end
+  end
+  resources :deliveries, only: [:index,:create,:destroy,:edit,:update]
+  namespace :admin do
+    resources :carts
+    resources :orders
+    resources :ordered_products
+  end
+
+  # Adminのrouting
+  namespace :admin do
+    get 'homes/top' => 'homes#top'
+    resources :genres, only: [:index,:edit,:create,:update]
+    resources :orders, only: [:index,:show,:update]
+    resources :ordered_products, only: [:update]
+    resources :products, only: [:index,:new,:create,:show,:edit,:update]
+    resources :users, only: [:index,:show,:edit,:update]
+  end
+
+end
