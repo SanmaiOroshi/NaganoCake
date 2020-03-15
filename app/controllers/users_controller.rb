bb6310b3fdf237d799bd0bd36a63be96f8c6efe0
takechihorn <takechihorn@gmail.com>
2020-03-15 04:47:46 +0000

    users_controller add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    def quit
+        
+    end
+
+    def update
+        @user = current_user
+        if @user.update(user_params)
+            redirect_to user_path
+        else
+            render "edit"
+        end
+    end
+    
+    def edit
+        @user = current_user
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :status)
+        end
+
 end
