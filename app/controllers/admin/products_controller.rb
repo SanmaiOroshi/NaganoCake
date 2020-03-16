7af96011377dee9e1b3bbfe2efb4e8ca564d137f
funeace <funeace@gmail.com>
2020-03-16 02:46:03 +0000

    [Add]Products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,32 @@
 class Admin::ProductsController < ApplicationController
+  def index
+    # 後々kaminariを導入
+    @products = Product.all
+    # binding.pry
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    # binding.pry
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    # binding.pry
+    product = Product.create(product_params)
+  end
+
+
+  def update
+
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:image,:name,:unit_price,:description,:genre_id)
+  end
+
 end
