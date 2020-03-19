d6166103b0f7b1e50faa706f76c67d220a80ab1d
funeace <funeace@gmail.com>
2020-03-19 08:05:37 +0000

    [Add]user_order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,72 @@
 class OrdersController < ApplicationController
+  def index
+      @orders = current_user.orders
+  end
+
+  def input
+    @user = current_user
+    @order = Order.new
+  end
+
+  def new
+    @carts = current_user.carts
+    @order = Order.new(orders_params)
+    @payment_method = params[:order][:payment_method]
+    @delivery_addr = params[:order][:delivery_addr]
+    case @delivery_addr
+    when "my_addr" then
+      @post_code = current_user.post_code
+      @address = current_user.address
+      @name = current_user.last_name + " " + current_user.first_name
+    when "registered_addr" then
+      delivery = Delivery.find(params[:order][:registered_address])
+      @post_code = delivery.post_code
+      @address = delivery.address
+      @name = delivery.name
+    when "new_addr" then
+      @post_code = params[:order][:new_post_cd]
+      @address = params[:order][:new_address]
+      @name = params[:order][:new_name]
+    end
+  end
+
+  def create
+    @order = current_user.orders.new(orders_params)
+
+    # 保存できるか確認
+    if @order.save
+      current_user.carts.each do |cart|
+        ordered_product = @order.ordered_products.create(
+          order_id: @order.id,
+          product_id: cart.product_id,
+          units: cart.units,
+          price: cart.price,
+          working_status: 1
+        )
+        Cart.destroy_all
+      end
+      redirect_to complete_orders_path
+    else
+      @post_code = order_params[:post_code]
+      @address = order_params[:address]
+      @name = order_params[:name]
+      @payment_method = params[:order][:payment_method]
+      @carts = current_user.carts
+      @order = Order.new(orders_params)
+      render 'new'
+    end
+  end
+
+  def complete
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+
+private
+  def orders_params
+    params.require(:order).permit(:user_id,:payment_method,:shipping_fee,:total_payment,:status,:name,:address,:post_code)
+  end
 end
