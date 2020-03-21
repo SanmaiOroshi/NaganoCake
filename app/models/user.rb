79c1079eb6fdaa8338342dd3ba0dcaeb5bac65a1
Zypressen-bouquet <sorakairi72@gmail.com>
2020-03-21 03:43:31 +0000

    [add]admin/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :carts, dependent: :destroy
 
+  validates :last_name,  presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana,  presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :phone_number, presence: true, format: {with: /\A\d{10}$|^\d{11}\z/}
+  validates :post_code,  presence: true, format: {with: /\A\d{7}\z/}
+  validates :address, presence: true
+
+
 end
