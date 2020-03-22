3b0d26528c3d9c65c425f62165094c457f79f990
funeace <funeace@gmail.com>
2020-03-22 05:07:49 +0000

    [Fix]Orders_layout,[Add]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
   Admin.create!(email: "admin@admin.com",password: "aaaaaa")
+  Genre.create!(genre:"ケーキ",display_status:true)
+  Genre.create!(genre:"プリン",display_status:true)
+  Genre.create!(genre:"焼き菓子",display_status:true)
+  Genre.create!(genre:"キャンディ",display_status:true)
+
+  5.times.each do |i|
+    Product.create!(genre_id: 1,name: "商品#{i+1}", unit_price: (i+1) * 300,status: true)
+  end
+
+  User.create(email:"user@user.com",password:"aaaaaa",
+    last_name:" 検証",first_name:"太郎",last_name_kana:"ケンショウ",
+    first_name_kana:"タロウ",post_code:"1111111",address:"東京都千代田区千代田１−１",
+    phone_number: "0001112222",status: true)
