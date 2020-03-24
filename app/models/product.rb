bff0212a2a03fdf0e8e50d2bcb6e78c9525d4faa
funeace <funeace@gmail.com>
2020-03-24 09:07:12 +0000

    [Fix]Admin_Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
 
-  # boolean は enum使うとfalse不具合が起きたので、ja.ymlで変換するだけで使用しない
-  # enum status: {chk_true: true, chk_false: false}
+  enum status: {true: true, false: false}
 
   has_many :carts, dependent: :destroy
   has_many :ordered_products, dependent: :destroy
